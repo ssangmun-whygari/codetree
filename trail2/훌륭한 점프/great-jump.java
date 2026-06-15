55ebfb8544900f6a3cdc8ef8b105718b9efe18dd
ssangmun-whygari <whygari4321@gmail.com>
2026-06-15 14:37:39 +0900

    [Passed] 훌륭한 점프 (132ms, 10MB)

--- trail2/훌륭한 점프/great-jump.java
+++ trail2/훌륭한 점프/great-jump.java
@@ -1,8 +1,12 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        // 1을 최대로 밟는다, 2를 최대로 밟는다, ... 100을 최대로 밟는다
+
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(), K = sc.nextInt();
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
         int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
@@ -11,38 +15,35 @@ public class Main {
         int[] sorted = new int[N];
         System.arraycopy(arr, 0, sorted, 0, N);
         Arrays.sort(sorted);
-        // System.out.println("sorted : " + Arrays.toString(sorted));
 
-        int ans = Integer.MAX_VALUE;
+        int ans = sorted[N - 1];
         for (int i = 0; i < N; i++) {
-
-            int cnt = 0;
-            int[] permit = new int[N];
-            Arrays.fill(permit, -1);
-            int max = sorted[i];
-
-            for (int j = 0; j < N; j++) {
-                if (arr[j] <= max) {
-                    permit[cnt++] = j;
-                }
-            }
-
-            // System.out.println("permit : " + Arrays.toString(permit));
-            
+            int limit = sorted[i];
+            int jump = 1;
             int curPos = 0;
-            for (int j = 0; j < cnt; j++) {
-                if (j == 0 && permit[0] != 0) break;
-                int nextPos = permit[j];
-                int dist = nextPos - curPos;
-                if (dist > K) break;
-                curPos = nextPos;
+            if (arr[curPos] > limit) continue;
+
+            boolean possible = true;
+            while (curPos != N - 1) {
+                possible = false;
+                for (int j = jump; j <= K; j++) {
+                    if (curPos + j >= N) break;
+                    if (arr[curPos + j] <= limit) {
+                        curPos = curPos + j;
+                        possible = true;
+                        break;
+                    }
+                }
+                if (!possible) break;
             }
 
-            if (curPos == N - 1)
-                ans = Math.min(ans, max);
-
+            if (possible) {
+                ans = limit;
+                break;
+            }
         }
 
         System.out.println(ans);
+
     }
 }
