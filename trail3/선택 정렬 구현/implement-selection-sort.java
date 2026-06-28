07ddcd32670a02cc37fac288dea1d3657032e4a9
ssangmun-whygari <whygari4321@gmail.com>
2026-06-28 19:02:09 +0900

    [Passed] 선택 정렬 구현 (137ms, 10MB)

---
+++ trail3/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        for (int i = 0; i < n - 1; i++) { // n - 1번 반복
+            int min = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+    }
+}
