4087870c32bf5a6674350927c83c33f4337af47c
ssangmun-whygari <whygari4321@gmail.com>
2026-07-01 17:36:02 +0900

    [Passed] 수열 조작 (170ms, 13MB)

---
+++ trail3/수열 조작/sequence-manipulation.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 1; i <= n; i++) {
+            dq.add(i);
+        }
+
+        while (dq.size() != 1) {
+            dq.pollFirst();
+            dq.addLast(dq.pollFirst());
+        }
+
+        System.out.println(dq.peekFirst());
+    }
+}
